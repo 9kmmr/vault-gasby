a5a58957aef5642943475193bb793251ad96e5cf
9kmmr <tunglam195@gmail.com>
2022-01-21 10:19:09 +0700

    fix image and style

--- src/components/header-desktop.js
+++ src/components/header-desktop.js
@@ -1,31 +1,47 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import  "./header-desktop.module.scss"
+import styles from "./header-desktop.module.scss"
 
 const HeaderDesktop = ({ siteTitle }) => (
   <header
-  class="header-container-desktop "
+  class={styles.header-container-desktop}
  >
-     <div class="header-container-row ">
-       <div class="header-container-search">
+     <div class={styles.header-container-row}>
+       <div class={styles.header-container-search}>
            <span></span>
-           <input id="MainSearchInput" value="" placeholder="Search Drivers, Teams, Venues..." spellcheck="false" class="search-input" />
+           <input id="MainSearchInput" value="" placeholder="Search Drivers, Teams, Venues..." spellcheck="false" class={styles.search-input} />
        </div>
-       <div class="header-container-search-data"></div>
+       <div class={styles.header-container-search-data}></div>
      </div>
-     <div class="header-container-row ">
+     <div class={styles.header-container-row}>
            <a href="">
-               <div class="header-site-logo ">
-                   <img src=""  alt=" Logo" width="210" height="52" />
+               <div class={styles.header-site-logo}>
+                <StaticImage
+                    src="../images/gatsby-astronaut.png"
+                    width={300}
+                    quality={95}
+                    formats={["auto", "webp", "avif"]}
+                    alt="A Gatsby astronaut"
+                    style={{ marginBottom: `1.45rem` }}
+                  />
+                   
                </div>
            </a>
-       <nav class="header-menu-dropdown ">
-         <ul class="menu-dropdown-contents ">            
-           <li class="dropdown-item item-active"><a href="">results</a></li>            
-           <li class="dropdown-item item-nonactive"><a href="">news</a></li>
-           <img src="../images/icons/delimiter.svg" alt="Delimiter" width="2" height="20" class="delimiter" />            
-           <li class="dropdown-item item-nonactive"><a href="">about us</a></li>
+       <nav class={styles.header-menu-dropdown}>
+         <ul class={styles.menu-dropdown-contents}>
+           <li class={styles.item-active}><a href="">results</a></li>            
+           <li class={styles.item-nonactive}><a href="">news</a></li>
+            <StaticImage
+              src="../images/icons/delimiter.svg"
+              width={2}
+              height={20}
+              quality={100}
+              formats={["auto", "webp", "avif"]}
+              alt="Delimiter"
+              class={styles.delimiter}
+            />           
+           <li class={styles.item-nonactive}><a href="">about us</a></li>
          </ul>
        </nav>
      </div>    
--- src/components/header-mobile.js
+++ src/components/header-mobile.js
@@ -1,18 +1,35 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import  "./header-mobile.module.scss"
+import styles from  "./header-mobile.module.scss"
 
 const HeaderMobile = ({ siteTitle }) => (
-    <header class="header-container-mobile">
-        <div class="header-container-row">
-            <div class="header-container-logo-mb">
+    <header class={styles.header-container-mobile}>
+        <div class={styles.header-container-row}>
+            <div class={styles.header-container-logo-mb}>
                 <a href="">
-                    <img src="" alt=" Logo" width="148" height="38" />
+                <StaticImage
+                    src="../images/gatsby-astronaut.png"
+                    width={148}
+                    height={38}
+                    quality={100}
+                    formats={["auto", "webp", "avif"]}
+                    alt="A Gatsby astronaut"                   
+                  />
+                   
                 </a>
             </div>
-        <div class="search-menu-mobile-container">
-            <img src="../images/icons/search-mobile.svg" width="16" height="16" alt="Search icon" class="search-mb-icon mb-icon" />
+        <div class={styles.search-menu-mobile-container}>
+            <StaticImage
+                    src="../images/icons/search-mobile.svg"
+                    width={16}
+                    height={16}
+                    quality={100}
+                    formats={["auto", "webp", "avif"]}
+                    alt="Search icon"    
+                    class="search-mb-icon mb-icon"        
+                  />
+            
         </div>
     </div>
     </header>
