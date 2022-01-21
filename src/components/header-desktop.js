60ae63f067127b04ed5a053ddd8f43aeeb65912a
9kmmr <tunglam195@gmail.com>
2022-01-21 10:55:18 +0700

    fix import

--- src/components/header-desktop.js
+++ src/components/header-desktop.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import PropTypes from "prop_types"
+import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import * as styles from "./header-desktop.module.scss"
 
